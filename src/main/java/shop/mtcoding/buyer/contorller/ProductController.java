97bd94d9bdb1c85aa5631197a6ff549b156edc7b
aozp73 <aozp73@naver.com>
2023-01-18 12:56:40 +0900

    세션-principal, jstl 조건식 문법으로 로그인 서비스 추가

--- src/main/java/shop/mtcoding/buyer/contorller/ProductController.java
+++ src/main/java/shop/mtcoding/buyer/contorller/ProductController.java
@@ -1,7 +1,12 @@
 package shop.mtcoding.buyer.contorller;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.buyer.model.UserRepository;
 
 @Controller
 public class ProductController {
@@ -10,4 +15,5 @@ public class ProductController {
     public String home() {
         return "product/list";
     }
+
 }
--- src/main/java/shop/mtcoding/buyer/model/UserRepository.java
+++ src/main/java/shop/mtcoding/buyer/model/UserRepository.java
@@ -3,6 +3,7 @@ package shop.mtcoding.buyer.model;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /*
  * UserRepository 기능 : 회원가입, 로그인, 회원탈퇴, 회원수정
@@ -27,21 +28,17 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserRepository {
     // 방법2) CRUD에 따라 생성
 
-    public int insert(String username, String password, String email);
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String email);
 
     public List<User> findAll();
 
     public User findById(int id);
 
-    public int updateById(int id, String password);
+    public int updateById(@Param("id") int id, @Param("password") String password);
 
     public int deleteById(int id);
 
-    // public int insert(String username, String password, String email);
+    public User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 
-    // public User login(String username, String password);
-
-    // public int delete(int id);
-
-    // public int updatePassword(int id, String password);
 }
